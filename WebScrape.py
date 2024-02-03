e9723d63ead623c8ab17d93a36fd26f826aa3142
Leosly <Leonardonigro11@gmail.com>
2024-02-02 19:15:24 -0500

    A lot of progress made

--- WebScrape.py
+++ WebScrape.py
@@ -25,18 +25,6 @@ def scrapeCity(cityNames, cityLinks):
     weatherReq = Request('https://weather.gc.ca/forecast/canada/index_e.html?id=ON', headers={'User-Agent': 'Mozilla/5.0'})
     weatherDoc = urlopen(weatherReq).read()
 
-    # empty loops and dictionaries to track and store info
-    cityNames = []
-    cityLinks = []
-    loop = 1
-    loop2 = 1
-    loop3 = 1
-    info = {}
-    timeDict = {}
-    temp = {}
-    weatherStuff = {}
-    weather = {}
-    conditions = {}
 
     soup = BeautifulSoup(weatherDoc, 'html.parser')
         # by giving it a class to look for you can get the specific information you need much easier
@@ -52,10 +40,54 @@ def scrapeCity(cityNames, cityLinks):
                 
         # In a production env this will be modularized, and its output routed to a file store where the main program will call on instead of having to rescrape every second
 
-        print(cityNames)
-        print(cityLinks)
+def scrapeWeather(links):
 
+     # sends a request to the website then returns the HTML infor to be sorted for usable info
+    weatherReq = Request(links, headers={'User-Agent': 'Mozilla/5.0'})
+    weatherDoc = urlopen(weatherReq).read()
 
-cityNames = []
-cityLinks = []
-scrapeCity(cityNames, cityLinks)
+    mainForecast = []
+
+    
+
+    soup = BeautifulSoup(weatherDoc, 'html.parser')
+        # by giving it a class to look for you can get the specific information you need much easier
+    
+    import re
+    elements = soup.find_all(class_="hidden-xs row no-gutters")
+    loop = 0
+
+    # Iterate through elements and filter strings
+    for element in elements:
+        # Iterate through the strings of the element
+        for string in element.stripped_strings:
+            # Check if the string matches the regex pattern
+            loop += 1
+            if re.match("^", string): 
+                # alright hear me out here
+                # the files are always formatted the same way and who tf is updating weather.gc.ca 
+                # so its not the worst idea to just pick out what I want instead of making a horribly complicated regex filter
+                # if you've dug this deep into my code to review it, you obv know what you're doing and we both know theres a better way to do this
+                # but im not getting paid for this and I'm a full time student so cherry picking our info is how we're doing it <3
+                if loop in {5,6,8,9,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34}:
+                    mainForecast.append(string)
+
+    loop = 0
+
+    futureForecast = [[],[],[],[],[],[],[]]
+
+    for i in range(7):
+        elements = soup.find_all(class_="div-column")[i]
+        for element in elements:
+            for string in element.stripped_strings:
+                # Check if the string matches the regex pattern
+                loop += 1
+                if re.match("^", string): 
+                    futureForecast[i].append(string)
+
+    print(futureForecast[1])
+    #['Sat', '3', 'Feb', '1', '°', 'C', 'Mainly sunny', 'Night', '-13', '°', 'C', 'A few clouds']  
+
+    #okay now i just want to steal their cute emojis
+
+    
